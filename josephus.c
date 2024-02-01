8f061bddda29acae176403a6001372298e3e43d3
George-Harpum <157149642+George-Harpum@users.noreply.github.com>
2024-02-01 16:15:16 +0000

    spellchecking

--- josephus.c
+++ josephus.c
@@ -7,7 +7,7 @@
 * 
 * There is the simple general solution (the formula for circle size n),
 * and the general formula (size n and step k; where in the above the step is 2, every 2nd soldier dies).
-* (the original josephus is for k=3, but I find the solution to k=2 particularly enjoyable.
+* The original josephus problem is for k=3, however the k=2 has an elegant solution I enjoy.
 */
 int josephus(int x); 
 int g(int n, int k);
@@ -22,22 +22,23 @@ int main() {
 }
 
 int josephus(int n) {
-  // for the case "k = 2" there exists an elegant binary solution
-  // the last member (x) for circle size (n) can be found with
-  // x = 2L + 1; 
-  // where L is; n = 2^m + L
-  // On paper, this is simply *moving* the highest bit to the end of the string.
-  // 11011 -> 10111
-  // In practice it's more involved:
-  // 01011 (removed highest power of 2)
-  // 10110 (x2, or, left shift 1)
-  // 10111 (+1)
+  /* for the case "k = 2" there exists an elegant binary solution
+  * the last member (x) for circle size (n) can be found with
+  * x = 2L + 1; 
+  * where L is; n = 2^m + L
+  * On paper, this is simply *moving* the highest bit to the end of the string.
+  * 11011 -> 10111
+  * In practice it's more involved:
+  * 01011 (removed highest power of 2)
+  * 10110 (x2, or, left shift 1)
+  * 10111 (+1)
+  */
   if (n == 0) {
       return 0;
   }
   int i = 0;
   while (n >> i != 1) {
-      i++; // finding highest hit
+      i++; // finding highest bit
   }
   int m = 1 << i; // making mask
   int out = ((n ^ m) << 1) + 1;
@@ -45,15 +46,17 @@ int josephus(int n) {
 }
 
 int g(int n, int k) {
-  // The general formula involves a simple form of recursion.
-  // After a single round, the circle size has decreased, and the round is repeated.
-  // Every game therefore consists one additional step over a smaller game.
-  // The game of size n can then be constructed from games size 1 -> n
+  /* The general formula involves a simple form of recursion.
+  * After a single round, the circle size has decreased, and the round is repeated.
+  * This logic also applies after a single 'kill' has occurred.
+  * Every game therefore consists one additional step over a smaller 'game'.
+  * The game of size n can then be constructed from game steps 1 -> n
+  */
   if (n == 0) {
       return 0;
   }
   int x = 0;
-  for (int i=1; i <= n; i++) {
+  for (int i=2; i <= n; i++) { // starts at 2 because x%1 = 0
       x += k;
       x %= i;
   }
